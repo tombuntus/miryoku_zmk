3e0e5463c97e33b1cc9aa22d09e3d927fb1cdf92
tombuntus <tombuntus@outlook.com>
2024-12-02 22:56:52 +0000

    feat: mods
    swap return for space on right thumb
    add return to right pinkie
    add backspace to right middle finger streched
    swap quote for semi

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -3,16 +3,28 @@
 
 #define XXX &none
 
+#define MIRYOKU_LAYER_BASE \
+&kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,               &kp U,             &kp I,             &kp O,             &kp P,             \
+U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,               U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, SEMI),   \
+U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,               &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
+U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, SPACE),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
+
+#define MIRYOKU_LAYER_NAV_VI \
+U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
+&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &kp LEFT,          &kp DOWN,          &kp UP,            &kp RIGHT,         &u_caps_word,      \
+U_NA,              &kp RALT,          &u_to_U_NUM,       &u_to_U_NAV,       U_NA,              &kp HOME,          &kp PG_DN,         &kp PG_UP,         &kp END,           &kp INS,           \
+U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp SPACE,         &kp BSPC,          &kp DEL,           U_NP,              U_NP
+
 #define MIRYOKU_LAYOUTMAPPING_LILY58( \
      K00, K01, K02, K03, K04,                K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,                K15, K16, K17, K18, K19, \
      K20, K21, K22, K23, K24,                K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,                K35, K36, K37, N38, N39 \
 ) \
-XXX       XXX  XXX  XXX  XXX  XXX                 XXX  XXX  XXX  XXX  XXX  XXX \
-XXX       K00  K01  K02  K03  K04                 K05  K06  K07  K08  K09  XXX \
-&kp ESC   K10  K11  K12  K13  K14                 K15  K16  K17  K18  K19  XXX \
-&kp LC(A) K20  K21  K22  K23  K24  XXX       XXX  K25  K26  K27  K28  K29  XXX \
+XXX       XXX  XXX  XXX  XXX  XXX                 XXX  XXX  &kp BSPC  XXX  XXX  XXX \
+XXX       K00  K01  K02  K03  K04                 K05  K06  K07       K08  K09  XXX \
+&kp ESC   K10  K11  K12  K13  K14                 K15  K16  K17       K18  K19  &kp RET \
+&kp LC(A) K20  K21  K22  K23  K24  XXX       XXX  K25  K26  K27       K28  K29  XXX \
                     XXX  K32  K34  K33       K35  K36  K37  XXX
 
 #if defined (MIRYOKU_KEYBOARD_LILY58)
@@ -23,10 +35,10 @@ XXX       K00  K01  K02  K03  K04                 K05  K06  K07  K08  K09  XXX \
      K20, K21, K22, K23, K24,                K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,                K35, K36, K37, N38, N39 \
 ) \
-XXX       XXX  XXX  XXX  XXX  XXX                 XXX  XXX  XXX  XXX  XXX  XXX \
-XXX       K00  K01  K02  K03  K04                 K05  K06  K07  K08  K09  XXX \
-&kp ESC   K10  K11  K12  K13  K14                 K15  K16  K17  K18  K19  XXX \
-&kp LC(A) K20  K21  K22  K23  K24  XXX       XXX  K25  K26  K27  K28  K29  XXX \
+XXX       XXX  XXX  XXX  XXX  XXX                 XXX  XXX  &kp BSPC  XXX  XXX  XXX \
+XXX       K00  K01  K02  K03  K04                 K05  K06  K07       K08  K09  XXX \
+&kp ESC   K10  K11  K12  K13  K14                 K15  K16  K17       K18  K19  &kp RET \
+&kp LC(A) K20  K21  K22  K23  K24  XXX       XXX  K25  K26  K27       K28  K29  XXX \
                     XXX  K32  K34  K33       K35  K36  K37  XXX
 
 #endif
